527cff7e08df55637b175c4f231c5511caa2f7a1
Love <78062729+love676@users.noreply.github.com>
2021-01-30 16:02:51 +0630

    Add files via upload

---
+++ Kill.py
@@ -0,0 +1,9 @@
+print ('''_
+| |    _____   _____
+| |   / _ \ \ / / _ \
+| |__| (_) \ V /  __/
+|_____\___/ \_/ \___|''')
+
+inp = input('Enter You Text : ')
+inp2 = int(input('Enter nuber : '))
+print(inp*inp2)
